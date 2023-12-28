5f77e83039b499265747d424c1845a689da9094c
lahcenrahlaoui <lahcen.rahlaoui@univ-tiaret.dz>
2023-12-28 23:24:29 +0100

    add fromtend endpoint to cors

--- index.js
+++ index.js
@@ -21,7 +21,11 @@ console.clear();
 app.use(express.static("public"));
 app.use(bodyParser.json({ limit: "30mb", extended: true }));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
-app.use(cors());
+app.use(
+    cors({
+        origin: ["https://social-media-client-blue.vercel.app/"],
+    })
+);
 
 // constants
 const MONGO_URL = process.env.MONGO_URL;
