5b5adf3344f32cfeb26dc1cc99a47f036b6d9400
lahcenrahlaoui <lahcen.rahlaoui@univ-tiaret.dz>
2023-12-29 00:06:34 +0100

    changfe link

--- index.js
+++ index.js
@@ -23,7 +23,7 @@ app.use(bodyParser.json({ limit: "30mb", extended: true }));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
 app.use(
     cors({
-        origin: "https://social-media-client-blue.vercel.app/",
+        origin: "https://social-media-client-blue.vercel.app/signin",
         credentials: true,
     })
 );
