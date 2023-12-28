101475f3f83a6045517bdc7771cb138bb3c325c6
lahcenrahlaoui <lahcen.rahlaoui@univ-tiaret.dz>
2023-12-28 23:26:46 +0100

    change link of the frontend

--- index.js
+++ index.js
@@ -23,7 +23,8 @@ app.use(bodyParser.json({ limit: "30mb", extended: true }));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
 app.use(
     cors({
-        origin: ["https://social-media-client-blue.vercel.app/"],
+        origin: "https://social-media-client-blue.vercel.app/",
+        credentials: true,
     })
 );
 
